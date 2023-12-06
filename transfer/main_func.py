825a66a0ae12acca5131d39f5c16c1f288669523
GodA1mighty <143071105+GodA1mighty@users.noreply.github.com>
2023-12-05 22:54:08 -0600

    Update main_func.py

--- transfer/main_func.py
+++ transfer/main_func.py
@@ -13,3 +13,36 @@ print(binary)
 user_input2 = input("Type Binary Here: ")
 text = "".join(chr(int(c, 2)) for c in user_input2.split(" "))
 print(text)
+
+
+# Get input from the user
+message = input("Enter a message: ")
+binary_values = []  # Create an empty list to store binary values
+# Iterate over each character in the message
+for character in message:
+    # Get the ASCII value of the current character
+    ascii_value = ord(character)
+    # Convert ASCII to binary
+    binary_representation = format(ascii_value, "b")
+    # Add the binary representation to the list
+    binary_values.append(binary_representation)
+# Join the binary values with a space in between
+binary = " ".join(binary_values)
+# Print the binary representation
+print("Binary representation:", binary)
+
+ascii_characters = []
+user_input = input("Type Binary Here: ")
+binary_substrings = user_input.split(" ")
+for character in binary_substrings:
+    decimal_value = int(character, 2)
+    ascii_character = chr(decimal_value)
+    ascii_characters.append(ascii_character)
+text = "".join(ascii_characters)
+print(text)
+    
+
+
+
+
+
