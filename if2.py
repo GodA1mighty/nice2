9622e2192bfed733709cc737a93de604410cf4e7
GodA1mighty <143071105+GodA1mighty@users.noreply.github.com>
2023-12-23 22:13:43 -0600

    Update if2.py

--- if2.py
+++ if2.py
@@ -1,12 +1,17 @@
-# Ask the user two true or false questions and convert the input to boolean values
-questions = ["Is Python an interpreted language?", "Is Python case-sensitive?"]
-answers = [input(q + " (True or False) ").lower() == "true" for q in questions]
+# Ask the user two true or false questions
+question1 = input("Is Python an interpreted language? (True or False) ")
+question2 = input("Is Python case-sensitive? (True or False) ")
+
+# Convert the user input to boolean values
+answer1 = question1.lower() == "true"
+answer2 = question2.lower() == "true"
 
 # Use an if-else statement to print a unique response for each combination of answers
-responses = [
-"You are wrong on both questions.", 
-"You are wrong on the first question, but correct on the second one.", 
-"You are correct on the first question, but wrong on the second one.", 
-"You are correct on both questions!"]
-
-print(responses[answers[0] * 2 + answers[1]])
+if answer1 and answer2:
+    print("You are correct on both questions!")
+elif answer1 and not answer2:
+    print("You are correct on the first question, but wrong on the second one.")
+elif not answer1 and answer2:
+    print("You are wrong on the first question, but correct on the second one.")
+else:
+    print("You are wrong on both questions.")
