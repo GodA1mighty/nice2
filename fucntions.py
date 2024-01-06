04b834ed49e4a96393c3c0fe151ee29f820e8801
GodA1mighty <143071105+GodA1mighty@users.noreply.github.com>
2024-01-05 19:32:37 -0600

    Update fucntions.py

--- fucntions.py
+++ fucntions.py
@@ -48,3 +48,36 @@ def create_name(first, last):
 
 full_name = create_name('Bro', 'Code')
 print(full_name)
+
+
+print('~~~~~~~~~~~~~~~~~~~~~')
+
+
+#step 1: There are five functions defined
+#Step 2: handle_payment Function: This function takes a payment_method as an argument. It uses a dictionary called 'dictionary' to map payment methods to their corresponding processing functions.
+#Step 3: The handle_payment function is called with “paypal” and “money” as arguments.
+#Step 4: When “paypal” is passed, the process_paypal function is executed, printing “Processing PayPal…”. When “money” is passed, since it’s not a key in the 'dictionary' dictionary, the default function process_error is executed, printing “Error. Please try again.”.
+
+def handle_payment(payment_method):
+    dictionary = {
+        "credit_card": process_credit_card,
+        "paypal": process_paypal,
+        "crypto": process_crypto,
+        "bank_transfer": process_bank_transfer,
+        "default": process_error
+    }
+    return dictionary.get(payment_method, dictionary["default"])()
+
+def process_credit_card():
+    print("Processing credit card...")
+def process_crypto():
+    print("Processing crypto...")
+def process_paypal():
+    print("Processing PayPal...")
+def process_bank_transfer():
+    print("Processing bank transfer...")
+def process_error():
+    print("Error. Please try again.")
+
+handle_payment("paypal")
+handle_payment("money")
