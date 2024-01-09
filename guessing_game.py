a3e3dc162e6f4e12e856cb625f25757d51d07a33
GodA1mighty <143071105+GodA1mighty@users.noreply.github.com>
2024-01-09 06:03:17 -0600

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -10,3 +10,26 @@ while guess_count < guess_limit:
 else:
     print('Sorry, You Failed...')
     
+
+print("\n---------------------------------------\n")
+
+
+import random
+
+low = 1
+high = 100
+guesses = 0
+number = random.randint(low, high)
+
+while True:
+    guess = int(input(f"Enter a number between ({low} - {high}): "))
+    guess += 1
+    
+    if guess < number:
+        print(f"{guess} is too low")
+    elif guess > number:
+        print(f"{guess} is too high")
+    else:
+        print(f"{guess} is correct!")
+        break
+print(f"This round took you {guesses} guesses.")
