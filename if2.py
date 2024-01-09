ad0e9309c566b6f668df5e0bc895d94ddf70243d
GodA1mighty <143071105+GodA1mighty@users.noreply.github.com>
2024-01-09 06:28:52 -0600

    Update if2.py

--- if2.py
+++ if2.py
@@ -1,1 +1,27 @@
+bad_credit = False
+good_credit = True
+price = 1000000
 
+if bad_credit:
+    down_payment = price * 0.2
+else:
+    down_payment = price * 0.1
+print(f'Down Payment: {down_payment}')
+
+
+print("\n---------------------------------\n")
+
+
+is_hot = True
+is_cold = False
+
+if is_hot:
+    print("It's a hot day")
+    print("Drink Plenty of Water")
+elif is_cold:
+    print("It's a cold day")
+    print("Bring a Sweater")
+else:
+    print('Normal Temperature Today')
+
+print("Enjoy your day")
