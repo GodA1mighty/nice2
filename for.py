b513b632f7de7e0848d3cb7d120bbe605bad074b
GodA1mighty <143071105+GodA1mighty@users.noreply.github.com>
2024-01-09 06:31:16 -0600

    Update for.py

--- for.py
+++ for.py
@@ -16,3 +16,28 @@ for value in range(1, 11):
     square = value ** 2
     squares.append(square)
 print(squares)
+
+
+print("\n----------------------------------\n")
+
+
+for item in 'Python': #each letter printed on it's own line
+    print(item)
+```````````````````````````````    
+for item in ['Mosh', 'John', 'Sarah']: #prints each string on it's own line
+    print(item)
+```````````````````````````````
+for item in [1, 2, 3, 4]: #prints numbers on it's own line
+    print(item)
+```````````````````````````````
+for item in range(10): #prints numbers on it's own line using range
+    print(item)
+```````````````````````````````
+for item in range(5, 10, 2): #prints numbers on it's own line using the range
+    print(item)              #while skipping the list by 2
+```````````````````````````````
+#shopping cart
+total = 0
+for price in [10, 20, 30]:
+    total += price
+print(f"Total: {total}")
