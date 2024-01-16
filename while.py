0036a77892fb4b63c3624a4600ad1d58232bb9c4
Get0n <143071105+Get0n@users.noreply.github.com>
2024-01-15 23:30:39 -0600

    Update while.py

--- while.py
+++ while.py
@@ -52,3 +52,47 @@ while num < 1 or num > 10:
     num = int(input("Enter a # between 1 - 10: "))
 
 print(f"You picked the number {num}")
+
+print()
+
+number = 0
+while number < 5:
+    print(number)
+    if number == 3:
+        break    #stops all the code
+    number = number + 1
+
+print()
+
+number = 0
+while number < 5:
+    print(number)
+    if number == 6:
+        break
+    number = number + 1
+else:    #activates only when the while loop is False
+    print("No longer < 5")
+
+print()
+
+#A1 infinite loop
+number = 0
+while number < 10:
+    print(number)
+    if number == 6:
+        continue    #when the if statement is true, it disregards the following code and repeats at the start of the while loop. so not being iterated by the number += 1 anymore
+    number = number + 1
+else:    #activates only when the while loop is False
+    print("No longer < 10")
+
+print()
+
+#A2 prevented infinite loop
+number = 0
+while number < 10:
+    number = number + 1
+    if number == 6:
+        continue
+    print(number)
+else:
+    print("No longer < 10")
